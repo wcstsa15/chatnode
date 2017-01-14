6eecbcf1635e3a4009fa34288c059e669481ec1b
Sigodin Solhrafn <wcstsa15@gmail.com>
2017-01-14 18:34:47 +0000

    Initial commit

---
+++ integrate.js
@@ -0,0 +1,27 @@
+// Integrate.js
+//
+const assert = require('assert');
+
+//Establish Infrastructure Integrity
+console.log(process.versions)
+
+var RequiredNodeVersion = '4.6.1';
+//assert(process.versions.node == '4.6.0', 'Wrong Node Version');
+assert(process.versions.node == RequiredNodeVersion, 'Wrong Node Version' + process.versions.node + "!=" + RequiredNodeVersion);
+//Establish Libraries/Dependencies Integrity
+
+//Establish Application Structure Integrity
+
+// Assert Modules
+
+// Assert Unit Tests
+
+// Assert Integration
+
+
+//Commit Pipeline
+
+//Build Pipline
+
+//Deploy Pipeline
+
